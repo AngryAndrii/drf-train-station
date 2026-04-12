fdd4d22f7c24c1443ce789b0dc16ca2f9215c43c
Andrii Chychkan <mywork1728@gmail.com>
2026-04-12 18:29:19 +0300

    added diagram and started models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        return (f"username: {self.username}; email: {self.email}")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.PositiveIntegerField()
+    journey = models.ManyToManyField("Journey")
+    order = models.ForeignKey(Order, blank=False, null=False, on_delete=models.CASCADE)
